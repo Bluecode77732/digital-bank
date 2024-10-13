27a30545b7fc9ce8bf6de3f35cf572b25f5003ee
Bluecode77732 <doookuuu@gmail.com>
2024-10-13 21:23:01 +0900

    The repeated Err occured during test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,21 @@
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+};
+
+module.exports = config;
+
 module.exports = {
-    preset: 'ts-jest',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
     testEnvironment: 'node',
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1'
-    }
+        '^@/(.*)$': '<rootDir>/$1',
+    },
 };
