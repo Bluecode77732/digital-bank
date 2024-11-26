e91035cf84d76f099f43b5dc24c6c2f62ee57230
Bluecode77732 <doookuuu@gmail.com>
2024-11-26 21:49:39 +0900

    new comment

--- src/db/factories/account.factory.ts
+++ src/db/factories/account.factory.ts
@@ -6,7 +6,7 @@ export default setSeederFactory(Account, (faker) => {   //#1 Using default is fo
     // Me : Account is just a class but can it be used like new Account()?
     // Peplexity : Yes. To create instances of Account, calling its constructor method to initialize the new object.
     const accnt = new Account();
-    accnt.accountNumber = `ACC-${faker.number.int({ min: 100000, max: 999999 })}`;
+    accnt.accountNumber = `ACC-${faker.number.int({ min: 100000, max: 999999 })}`;  //The `int` type : int (options?: number | { min?: number; max?: number; }): number
     accnt.accountType = faker.helpers.arrayElement(['savings', 'checking', 'investment']);
     accnt.balance = faker.number.float({ min: 0, max: 10000, precision: 0.01 });
     accnt.currency = 'USD';
